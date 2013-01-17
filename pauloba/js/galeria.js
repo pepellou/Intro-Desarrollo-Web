fb841b7d71599ee6d98b7b624ae0e673e40829fb
Pau <pau@XPS.(none)>
2013-01-17 20:02:22 +0100

    modificacion

---
+++ pauloba/js/galeria.js
@@ -0,0 +1,20 @@
+$(function() {
+	
+	$('#galeria a').lightBox({	
+			txtOf : '/',
+			txtImage :'', 
+			imageBtnPrev :'img/lightbox-btn-prev.gif', 
+			imageBtnNext : 'img/lightbox-btn-next.gif',
+			imageBtnClose : 'img/lightbox-btn-close.gif',
+			imageLoading : 'img/lightbox-ico-loading.gif'
+		}); // Select all links in object with gallery ID
+	$('#galeria2 a').lightBox({	
+			txtOf : '/',
+			txtImage :'', 
+			imageBtnPrev :'img/lightbox-btn-prev.gif', 
+			imageBtnNext : 'img/lightbox-btn-next.gif',
+			imageBtnClose : 'img/lightbox-btn-close.gif',
+			imageLoading : 'img/lightbox-ico-loading.gif'
+		}); // Select all links in object with gallery ID
+	
+});
