8fd3f3b35adcf5c97b33f896716115802df187f9
Pepe Doval <pepellou@gmail.com>
2013-01-22 19:11:40 +0100

    Move styles to CSS file

--- pepellou/process-login.php
+++ pepellou/process-login.php
@@ -40,38 +40,7 @@ if (file_exists($user_data_file)) {
 <head>
 	<title>My favourite videos</title>
 	<link href='http://fonts.googleapis.com/css?family=Swanky+and+Moo+Moo' rel='stylesheet' type='text/css'>
-	<style>
-		body {
-			font-family: 'Swanky and Moo Moo', cursive;
-			font-size: 14pt;
-			background-color: #e8e8e8; 
-		}
-		h1 { 
-			font-size: 1.5em;
-			color: #555; 
-			background-color: #c8c8c8; 
-			padding: 3% 2%;
-			border: 2px dotted #aaa;
-			text-align: center;
-		}
-		p {
-			text-align: justify;
-			line-height: 1.2em;
-		}
-		a {
-			color: black;
-			text-decoration: underline;
-			font-weight: bold;
-		}
-		a:hover { color: #ccc; }
-		h1 a { text-decoration: none; }
-		h1 a:hover { color: #888; }
-
-		div.main {
-			width: 1000px;
-			margin: auto;
-		}
-	</style>
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<div class="main">
